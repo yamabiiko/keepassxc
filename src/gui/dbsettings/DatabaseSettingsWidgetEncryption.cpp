11b3595e2df74d06ca8c8672a7d677995fca94ad
Xavier Valls <xaviervallspla@gmail.com>
2021-05-18 08:22:47 +0200

    disable decryption time sliding bar when advanced settings active

--- src/gui/dbsettings/DatabaseSettingsWidgetEncryption.cpp
+++ src/gui/dbsettings/DatabaseSettingsWidgetEncryption.cpp
@@ -381,6 +381,7 @@ void DatabaseSettingsWidgetEncryption::setAdvancedMode(bool advanced)
     }
 
     m_ui->advancedSettings->setVisible(advanced);
+    m_ui->decryptionTimeSlider->setEnabled(!advanced);
     config()->set(Config::GUI_AdvancedSettings, advanced);
 }
 
